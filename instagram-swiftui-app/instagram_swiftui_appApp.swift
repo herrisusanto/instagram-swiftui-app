1673fcee06c9c24934ccee81e47d3d255d97a6c7
Heri Susanto <herisusanto.id@gmail.com>
2024-01-23 19:20:52 +0700

    Initial Commit

---
+++ instagram-swiftui-app/instagram_swiftui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_swiftui_appApp.swift
+//  instagram-swiftui-app
+//
+//  Created by loratech on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct instagram_swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
