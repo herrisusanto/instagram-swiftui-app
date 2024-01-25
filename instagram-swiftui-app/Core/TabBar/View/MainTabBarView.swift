8b8a8f9265621bbf8bba73380df6026292aa98e7
Heri Susanto <herisusanto.app@gmail.com>
2024-01-25 21:30:07 +0700

    Add Upload Post View Add Upload Post View Model

--- instagram-swiftui-app/Core/TabBar/View/MainTabBarView.swift
+++ instagram-swiftui-app/Core/TabBar/View/MainTabBarView.swift
@@ -8,28 +8,49 @@
 import SwiftUI
 
 struct MainTabBarView: View {
+    @State private var selectedIndex = 0
     var body: some View {
-        TabView{
+        TabView(selection: $selectedIndex){
             FeedView()
+                .onAppear{
+                    selectedIndex = 0
+                }
                 .tabItem {
                     Image(systemName: "house")
                 }
+                .tag(0)
             SearchView()
+                .onAppear{
+                    selectedIndex = 1
+                }
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                 }
-            Text("Upload Post")
+                .tag(1)
+            UploadPostView(tabIndex: $selectedIndex)
+                .onAppear{
+                    selectedIndex = 2
+                }
                 .tabItem {
                     Image(systemName: "plus.square")
                 }
+                .tag(2)
             Text("Notifications")
+                .onAppear{
+                    selectedIndex = 3
+                }
                 .tabItem {
                     Image(systemName: "heart")
                 }
+                .tag(3)
             CurrentUserProfileView(user: User.MOCK_USERS[0])
+                .onAppear{
+                    selectedIndex = 4
+                }
                 .tabItem {
                     Image(systemName: "person")
                 }
+                .tag(4)
             
         }
         .accentColor(.black)
---
+++ instagram-swiftui-app/Core/UploadPost/View/UploadPostView.swift
@@ -0,0 +1,68 @@
+//
+//  UploadPostView.swift
+//  instagram-swiftui-app
+//
+//  Created by loratech on 25/01/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct UploadPostView: View {
+    @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @StateObject  var viewModel = UploadPostViewModel()
+    @Binding var tabIndex: Int
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button{
+                    caption = ""
+                    viewModel.selectedImage = nil
+                    viewModel.postImage = nil
+                    tabIndex = 0
+                }label: {
+                    Text("Cancel")
+                }
+                
+                Spacer()
+                
+                Text("New Post")
+                
+                Spacer()
+                
+                Button{
+                    print("upload post")
+                }label:{
+                    Text("Upload")
+                }
+            }
+            .padding(.horizontal)
+            
+            HStack(spacing: 8) {
+                if let image = viewModel.postImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
+                
+                
+                TextField("Enter your caption...", text: $caption)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .onAppear{
+            imagePickerPresented.toggle()
+        }
+        .photosPicker(isPresented: $imagePickerPresented, selection: $viewModel.selectedImage)
+    }
+}
+
+#Preview {
+    UploadPostView(tabIndex: .constant(0))
+}
